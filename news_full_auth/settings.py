19bc4a4d3d9404e656049f12740ab924e7ddcd6f
pGUPT4 <pratbro1@gmail.com>
2025-04-28 04:33:05 +0530

    Update settings

--- news_full_auth/settings.py
+++ news_full_auth/settings.py
@@ -205,7 +205,7 @@ AUTH_COOKIE_SAMESITE = 'None'
 
 CORS_ALLOWED_ORIGINS = getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:3000, http://127.0.0.1:3000'
+    'http://localhost:3000, http://localhost:3001, http://127.0.0.1:3000'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
